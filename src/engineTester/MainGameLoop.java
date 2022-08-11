6b8491a795a32afd53c2906fbe32db64f4a21bfb
gjaskula99 <81091594+gjaskula99@users.noreply.github.com>
2022-08-11 20:32:54 +0200

    Initial 14

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -40,7 +40,12 @@ public class MainGameLoop {
 		
 		Light light = new Light(new Vector3f(20000,20000,2000),new Vector3f(1,1,1));
 		
-		Terrain terrain = new Terrain(0,0,loader,new ModelTexture(loader.loadTexture("grass")));
+		//Original
+		//Terrain terrain = new Terrain(0,0,loader,new ModelTexture(loader.loadTexture("grass")));
+		//Terrain terrain2 = new Terrain(1,0,loader,new ModelTexture(loader.loadTexture("grass")));
+		//
+		Terrain terrain = new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("grass")));
+		Terrain terrain2 = new Terrain(-1,-1,loader,new ModelTexture(loader.loadTexture("grass")));
 		
 		Camera camera = new Camera();	
 		MasterRenderer renderer = new MasterRenderer();
@@ -49,6 +54,7 @@ public class MainGameLoop {
 			camera.move();
 			
 			renderer.processTerrain(terrain);
+			renderer.processTerrain(terrain2);
 			for(Entity entity:entities){
 				renderer.processEntity(entity);
 			}
--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -5,63 +5,49 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
 	
-	private Vector3f position = new Vector3f(400,5,400);
+	private Vector3f position = new Vector3f(0,5,0);
 	private float pitch = 10;
-	private float yaw = 0;
+	private float yaw ;
 	private float roll;
 	
 	public Camera(){}
 	
-	public void printCoords()
-	{
-		System.out.println("Coordinates: " + this.position);
-	}
-	
-	public void move()
-	{
+	public void move(){
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
 			position.z-=0.2f;
-			printCoords();
 		}
+		//ADDED
 		if(Keyboard.isKeyDown(Keyboard.KEY_S)){
 			position.z+=0.2f;
-			printCoords();
 		}
+		//
 		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
 			position.x+=0.2f;
-			printCoords();
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
 			position.x-=0.2f;
-			printCoords();
 		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_Q)){
-			yaw-=1;
-			printCoords();
+		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)){
+			position.y+=0.2f;
 		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_E)){
-			yaw+=1;
-			printCoords();
+		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)){
+			position.y-=0.2f;
 		}
 	}
 
-	public Vector3f getPosition()
-	{
+	public Vector3f getPosition() {
 		return position;
 	}
 
-	public float getPitch()
-	{
+	public float getPitch() {
 		return pitch;
 	}
 
-	public float getYaw()
-	{
+	public float getYaw() {
 		return yaw;
 	}
 
-	public float getRoll()
-	{
+	public float getRoll() {
 		return roll;
 	}
 	
