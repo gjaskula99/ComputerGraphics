2528108f8fbaefdeba53cd642776602d749382e9
gjaskula99 <81091594+gjaskula99@users.noreply.github.com>
2022-09-06 17:04:33 +0200

    Chuj działa

--- src/textures/ModelTexture.java
+++ src/textures/ModelTexture.java
@@ -7,13 +7,58 @@ public class ModelTexture {
 	private float shineDamper = 1;
 	private float reflectivity = 0;
 	
+	private boolean hasTransparency = false;
+	private boolean useFakeLightnig = false; 
+	
+	private int numberOfRows = 1;
+			
+	
 	public ModelTexture(int texture){
 		this.textureID = texture;
 	}
 	
+	
+	
+	public boolean isUseFakeLightnig() {
+		return useFakeLightnig;
+	}
+
+
+
+	public void setUseFakeLightnig(boolean useFakeLightnig) {
+		this.useFakeLightnig = useFakeLightnig;
+	}
+
+
+
+	public int getNumberOfRows() {
+		return numberOfRows;
+	}
+
+
+
+	public void setNumberOfRows(int numberOfRows) {
+		this.numberOfRows = numberOfRows;
+	}
+
+
+
+	public boolean isHasTransparency() {
+		return hasTransparency;
+	}
+
+
+
+	public void setHasTransparency(boolean hasTransparency) {
+		this.hasTransparency = hasTransparency;
+	}
+
+
+
 	public int getID(){
 		return textureID;
 	}
+	
 
 	public float getShineDamper() {
 		return shineDamper;
