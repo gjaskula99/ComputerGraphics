ab2be8e37470314b7362560e5ea8613d1b2fdcf9
gjaskula99 <81091594+gjaskula99@users.noreply.github.com>
2022-09-11 23:09:30 +0200

    1.0-dev

---
+++ README.md
@@ -0,0 +1,24 @@
+# Computer Graphics Project
+
+Projekt z grafiki komputerowej.
+Przedstawia wygenerowaną proceduralnie mapę o rozmiarze 800x800. Całość jest pokryta teksturami w oparciu o blendmapę, elewacja terenu wyliczona na podstawie rastrowej mapy wysokości. Mapa pokryta obiektami, których współrzędne generowane są pseudolosowo i ładowane z plików OBJ. Na całość nałożony skybox. Po mapie porusza się model gracza sterowany przez WSAD i spację, regulacja odległości kamery możliwa poprzez kółko myszy.
+
+![Przechwytywanie](https://user-images.githubusercontent.com/81091594/189534903-6e21b0bc-77ec-4f81-b7ad-b7e1be22d9ce.PNG)
+
+## Backend
+
+Projekt napisany w Javie z wykorzystaniem biblioteki Light-weight Java Game Library (https://www.lwjgl.org).
+Do uruchomienia w IDE niezbędne załączenie bibliotek zewnętrznych - umieszczone w /lib/jars oraz /lib/natives pod architekturę amd64. "Main" stanowi klasa EngineTester.
+W przypadku Eclipsa:
+1. Zaimportowanie projektu z kodu źródłowego.
+2. Dodanie plików .jar LWJGL do projektu.
+
+![JARs](https://user-images.githubusercontent.com/81091594/189535457-97d840ea-4109-4683-8b59-75971be5cfc9.PNG)
+
+3. Wskazanie ścieżki natives w ustawieniach projektu.
+
+![natives](https://user-images.githubusercontent.com/81091594/189535789-8d64cbb4-c929-4d36-a602-149af133e958.PNG)
+
+Testy prowadzono na:
+- JRE 8 na Windows 10 (amd64)
+- JRE 17.0.3 na macOS 12.5 (arm64)
---
+++ src/entities/Player.java
@@ -0,0 +1,106 @@
+package entities;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.util.vector.Vector3f;
+
+import models.TexturedModel;
+import renderEngine.DisplayManager;
+import terrains.Terrain;
+
+public class Player extends Entity{
+
+	private static final float RUN_SPEED = 20;
+	private static final float TURN_SPEED = 120;
+	private static final float GRAVITY = -80;
+	private static final float JUMP_POWER = 40;
+	
+	private static final float TERRAIN_HEIGHT = 0;
+		
+	private float currentSpeed = 0;
+	private float currentTurnSpeed = 0;
+	private float upwardsSpeed = 0; 
+	
+	private boolean isInAir = false;
+	
+	
+	public Player(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
+		super(model, position, rotX, rotY, rotZ, scale);
+		
+	}
+	
+	public void move(Terrain terrain) {
+		checkInputs();
+		super.increaseRotation(0, currentTurnSpeed * DisplayManager.getFrameTimeSeconds(), 0);
+		float distance = currentSpeed * DisplayManager.getFrameTimeSeconds();
+		float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY())));
+		float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY())));
+		super.increasePosition(dx, 0, dz);
+		upwardsSpeed += GRAVITY * DisplayManager.getFrameTimeSeconds();
+		super.increasePosition(0, upwardsSpeed*DisplayManager.getFrameTimeSeconds(), 0);
+		float terrainHeight = terrain.getHeightOfTerrain(super.getPosition().x, super.getPosition().z);
+		if(super.getPosition().y < terrainHeight) {
+			upwardsSpeed = 0;
+			isInAir = false;
+			super.getPosition().y = terrainHeight;
+		}
+		
+		//"Invisible wall"
+		if(this.getPosition().x < 0)
+		{
+			Vector3f temp = new Vector3f(0, this.getPosition().y, this.getPosition().z);
+			this.setPosition(temp);
+		}
+		if(this.getPosition().x > 800)
+		{
+			Vector3f temp = new Vector3f(800, this.getPosition().y, this.getPosition().z);
+			this.setPosition(temp);
+		}
+		if(this.getPosition().z < -800)
+		{
+			Vector3f temp = new Vector3f(this.getPosition().x, this.getPosition().y, -800);
+			this.setPosition(temp);
+		}
+		if(this.getPosition().z > 0)
+		{
+			Vector3f temp = new Vector3f(this.getPosition().x, this.getPosition().y, 0);
+			this.setPosition(temp);
+		}
+	}
+	
+	private void jump() {
+		if(!isInAir) {
+		this.upwardsSpeed = JUMP_POWER;
+		isInAir = true;
+		}
+		
+	}
+	
+	private void checkInputs() {
+		if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
+			this.currentSpeed = RUN_SPEED;
+			
+		}else if(Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			this.currentSpeed = -RUN_SPEED; 
+		
+		}else {
+			this.currentSpeed = 0;
+		}
+		
+		if(Keyboard.isKeyDown(Keyboard.KEY_D)) {
+			this.currentTurnSpeed = -TURN_SPEED;
+			
+		}else if(Keyboard.isKeyDown(Keyboard.KEY_A)) {
+			this.currentTurnSpeed = TURN_SPEED;
+			
+		}else {
+			this.currentTurnSpeed = 0;
+		}
+	
+		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)){
+			jump();
+		}
+		
+		
+	}
+
+}
